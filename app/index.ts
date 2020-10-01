d68e75019ce14a50945000747d670c14e1dd034e
Dóró Szilárd <szilard.doro@bishop.hu>
2020-10-01 23:24:37 +0200

    Added support for colorisation, timestamps and a common format Messages logged to the console are colorised based on their severity, have a timestamp and are in the requested format

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,4 @@
 import { app, constants } from './config'
 import logger from './logger'
 
-app.listen(constants.PORT, () => {
-    logger.info(`Application listening on port ${constants.PORT}.`)
-})
+app.listen(constants.PORT, () => logger.info(`Application listening on port ${constants.PORT}.`))
